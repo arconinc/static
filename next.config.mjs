c9eb05c5dfa51cc97e4867491d8c342269579305
mchristianson <christianson.matt@gmail.com>
2026-06-24 13:30:31 -0500

    Remove large wp-content folder

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
+const wpContentBucketBase =
+  'https://hrjlnxoyindmmacdjbsn.supabase.co/storage/v1/object/public/public-images'
+
 const nextConfig = {
   trailingSlash: true,
   async rewrites() {
     return {
       beforeFiles: [
+        {
+          source: '/wp-content/:path*',
+          destination: `${wpContentBucketBase}/wp-content/:path*`
+        },
         {
           source: '/',
           destination: '/index.html'
