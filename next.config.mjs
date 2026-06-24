f161bf5968d6c987efd1d5fb9b8d202b039a2184
mchristianson <christianson.matt@gmail.com>
2026-06-24 11:25:06 -0500

    Use react/next.js for the entire site

--- next.config.mjs
+++ next.config.mjs
@@ -1,52 +1,19 @@
-const legacyDirectories = [
-  '2024-dpr-golf-tournament',
-  'apparel',
-  'arcon-customer-feedback',
-  'author',
-  'awards',
-  'blog',
-  'contact',
-  'custom-apparel',
-  'custom-carhartt',
-  'custom-drinkware',
-  'dbia-golf-tournament',
-  'design',
-  'dpr-golf-tournament',
-  'dt_gallery',
-  'dt_logos',
-  'dt_slideshow',
-  'e-commerce',
-  'fitness',
-  'hall-threads-2-0-signup',
-  'holiday',
-  'media',
-  'package',
-  'portfolio',
-  'print',
-  'project',
-  'project-category',
-  'promo',
-  'rapala-logo-lure-order-form',
-  'rapala-lure-order-questions',
-  'test',
-  'tss_data',
-  'tss_slider',
-  'wp-content',
-  'wp-includes',
-  'wpa-stats-type'
-]
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
-  outputFileTracingIncludes: {
-    '/*': [
-      './*.html',
-      './*.xml',
-      './*.xsl',
-      './*.txt',
-      ...legacyDirectories.map((directory) => `./${directory}/**/*`)
-    ]
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/',
+          destination: '/index.html'
+        },
+        {
+          source: '/:path*/',
+          destination: '/:path*/index.html'
+        }
+      ]
+    }
   }
 }
 
--- wp-content/plugins/testimonial-add/lpp_color_picker.js
+++
@@ -1,3 +0,0 @@
-jQuery(document).ready(function(){
-			    jQuery('.tss-color-picker').wpColorPicker();
-			});
